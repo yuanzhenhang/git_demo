bafc95c08db0b13647a6de58cbf15c8f43cca123
root <root@qq.com>
2018-11-14 20:59:07 +0800

    主分支第一次提交

---
+++ src/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @Author:SAMSUNG
+ * @Time:2018/11/14 20:56
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("断剑重铸之日");
+    }
+}
